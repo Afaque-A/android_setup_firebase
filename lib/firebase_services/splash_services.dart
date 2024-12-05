1644b344e1b05c98c6ff3f51d4493c362703fbec
Afaque Ahmed <145118453+Afaque-A@users.noreply.github.com>
2024-12-06 00:39:02 +0500

    authentication

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,12 +1,23 @@
 import 'dart:async';
 
 import 'package:android_setup_firebase/ui/auth/login_screen.dart';
+import 'package:android_setup_firebase/ui/post/post_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class SplashServices {
   void isLogin() {
-    Timer(const Duration(seconds: 3), () {
-      Get.to(const LoginScreen());
-    });
+    final auth = FirebaseAuth.instance;
+    final user = auth.currentUser;
+
+    if (user != null) {
+      Timer(const Duration(seconds: 3), () {
+        Get.to(const PostScreen());
+      });
+    } else {
+      Timer(const Duration(seconds: 3), () {
+        Get.to(const LoginScreen());
+      });
+    }
   }
 }
--- lib/ui/post/post_screen.dart
+++ lib/ui/post/post_screen.dart
@@ -1,4 +1,7 @@
+import 'package:android_setup_firebase/ui/auth/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -8,11 +11,32 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title:  const Text('Post_Screen'),
+        appBar: PreferredSize(
+      preferredSize: const Size.fromHeight(60), // AppBar height
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: AppBar(
+          backgroundColor: Colors.cyan,
+          automaticallyImplyLeading: false,
+          title: const Text('Home_ Screen'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  auth.signOut().then((value) {
+                    Get.to(() => LoginScreen());
+                  });
+                },
+                icon: Icon(Icons.logout))
+          ],
+        ),
+      ),
     ));
   }
 }
